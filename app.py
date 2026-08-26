1e2f8d65863dbd123a5abf9110d42b7f399b360c
chiushengfuchiu-hue <chiushengfu.chiu@gmail.com>
2026-08-26 16:40:26 +0800

    Update greeting from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ VERSES_FILE = "verses.csv"
 SCHEDULE_RECORD_FILE = "schedule_records.csv"
 ATTENDANCE_FILE = "attendance_records.csv"
 SCHEDULE_DIR = "schedules_img"
-ADMIN_PASSWORD = "church_admin"
+ADMIN_PASSWORD = "11190928"
 
 PLAN_YEAR = 2 
 
