aa31853efdfe125fc9010d7b121e6b53043cfacc
chiushengfuchiu-hue <chiushengfu.chiu@gmail.com>
2026-09-02 10:24:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -765,9 +765,9 @@ with tab_resource:
         st.markdown(
             """
             <div style="background-color: #F8FAFC; padding: 15px; border-radius: 10px; border: 2px solid #10B981;">
-                <b>📘 新約經卷導覽與架構</b><br>
-                <p style="font-size: 14px; color: #4B5563; margin-top: 5px;">掌握四福音書、使徒行傳、書信及啟示錄的背景重點。</p>
-                <a href="https://zh.wikipedia.org/wiki/新約聖經" target="_blank" style="font-weight: bold; color: #059669;">👉 檢視新約經卷介紹</a>
+                <b>📘 認識聖經各經卷SoundOn</b><br>
+                <p style="font-size: 14px; color: #4B5563; margin-top: 5px;">認識每卷書背景與主題，明白上帝的本質與作為。</p>
+                <a href="https://player.soundon.fm/p/49f6e2a8-a4c8-463c-9c97-7d4e4a8a4188" target="_blank" style="font-weight: bold; color: #059669;">👉 檢視新約經卷介紹</a>
             </div>
             """,
             unsafe_allow_html=True
